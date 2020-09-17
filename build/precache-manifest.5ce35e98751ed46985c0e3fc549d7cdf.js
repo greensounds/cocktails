2d4156667c71746a5540e0f11bf4817ff5406a0c
greensounds <jeda.1992@hotmail.com>
2020-09-17 11:07:04 -0500

    first commit

---
+++ build/precache-manifest.5ce35e98751ed46985c0e3fc549d7cdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118bc76266197f66a4c580682c8454d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "439c051780e466f26aff",
+    "url": "/static/css/main.7ca4dffd.chunk.css"
+  },
+  {
+    "revision": "2c7b8d66825430a56f78",
+    "url": "/static/js/2.4821d250.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/static/js/2.4821d250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "439c051780e466f26aff",
+    "url": "/static/js/main.079e8971.chunk.js"
+  },
+  {
+    "revision": "1dedcc35924429767ab8",
+    "url": "/static/js/runtime-main.c893872a.js"
+  }
+]);
